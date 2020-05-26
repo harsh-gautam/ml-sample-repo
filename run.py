733352f1b4f100a6258acc31ea5c6e8308d1dbb4
Harsh Gautam <hgautam208@gmail.com>
2020-05-26 14:47:44 +0530

    Tweaked the code

--- run.py
+++ run.py
@@ -50,6 +50,8 @@ model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPool2D(pool_size=(2, 2)))
 model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
 
 # Flattening layer
 model.add(Flatten())
@@ -58,6 +60,8 @@ model.add(Flatten())
 model.add(Dense(units=512, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(units=256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(units=128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(units=1, activation='sigmoid'))
 
